6e5cb02ec3f11217881ee16b1b045cd95c8d6320
dwq <1306857779@qq.com>
2019-03-12 18:40:58 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
     //用户注册页
     public function create()
     {
     	return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+    	return view('users.show',compact('user'));
     }
 }
