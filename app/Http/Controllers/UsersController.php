c23819e81abf974c136ed615de83fb20678401c3
dwq <1306857779@qq.com>
2019-03-13 12:16:56 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,8 +12,19 @@ class UsersController extends Controller
     	return view('users.create');
     }
 
+    //个人信息展示页
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
+    }
+
+    //用户注册提交处理
+    public function store(Request $request){
+    	$this->validate($request,[
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|unique:users|max:255',
+    		'password' => 'required|confirmed|min:6'
+    	]);
+    	return;
     }
 }
