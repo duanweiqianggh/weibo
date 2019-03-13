66ca81b0d82ee79ad7f742ce2be0868960a8abc5
dwq <1306857779@qq.com>
2019-03-13 20:37:43 +0800

    登录验证 创建会话

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,14 @@
 });*/
 
 //git config core.autocrlf false 稍后研究下此命令
+//共享页面
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
+//用户注册
 Route::get('/signup', 'UsersController@create')->name('signup');
-
 Route::resource('users','UsersController');
+//用户登录及退出
+Route::get('/login','SessionsController@create')->name('login');
+Route::post('/login','SessionsController@store')->name('login');
+Route::delete('/logout','SessionsController@destroy')->name('logout');
