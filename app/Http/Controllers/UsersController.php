80d59ab7e711486cb803ed2266c289a590515e40
David <3075018691@qq.com>
2019-03-20 15:52:34 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,27 +12,85 @@ class UsersController extends Controller
     	return view('users.create');
     }
 
+    //用户注册提交处理
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        //此处注册成功后 直接为用户登录
+        Auth::login($user);
+        session()->flash('success','欢迎您加入大T博客');
+        return redirect()->route('users.show',[$user]);
+    }
+
     //个人信息展示页
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
     }
 
-    //用户注册提交处理
-    public function store(Request $request){    	
-    	$this->validate($request,[
-    		'name' => 'required|max:50',
-    		'email' => 'required|email|unique:users|max:255',
-    		'password' => 'required|confirmed|min:6'
-    	]);
-    	$user = User::create([
-    		'name' => $request->name,
-    		'email' => $request->email,
-    		'password' => bcrypt($request->password)
-    	]);
-        //此处注册成功后 直接为用户登录
-        Auth::login($user);
-    	session()->flash('success','欢迎您加入大T博客');
-    	return redirect()->route('users.show',[$user]);
+    //个人信息编辑页
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
     }
+
+    //个人信息编辑提交处理
+
+    /**
+     * @param User $user
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $data = [];
+        $data['name'] = $request['name'];
+        if($request->password)
+        {
+            $data['password'] = $request->password;
+        }
+        $user->update($data);
+        session()->flash('success','个人资料更新成功!');
+        return redirect()->route('users.show',$user->id);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
